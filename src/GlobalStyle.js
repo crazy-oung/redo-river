7c7850e3020e9299322e34781462e72fb4ce2dec
crazy-oung <hdayoungh@gmail.com>
2021-08-12 19:57:55 +0900

    feat: Add font weight 500

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -1,7 +1,9 @@
 import styled, { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
-import { DefaultContentWidth } from "./static/Shared/commonStyles";
-
+import {
+  DefaultContentWidth,
+  MobileViewWidth,
+} from "./static/Shared/commonStyles";
 export const GlobalStyle = createGlobalStyle`
   ${reset}
   @font-face {
@@ -9,6 +11,12 @@ export const GlobalStyle = createGlobalStyle`
       font-weight: normal; 
       font-style: normal;
       src: url(assets/font/NotoSansCJKKR/NotoSansCJKkr-Regular.otf) format('opentype');
+  }
+  @font-face {
+      font-family: 'NotoSansCJKKR';
+      font-weight: 500; 
+      font-style: normal;
+      src: url(assets/font/NotoSansCJKKR/NotoSansCJKkr-Medium.otf) format('opentype');
   }
   @font-face {
       font-family: 'NanumSquareRound';
@@ -22,13 +30,6 @@ export const GlobalStyle = createGlobalStyle`
       font-style: normal;
       src: url(assets/font/NanumSquareRound/NanumSquareRoundOTFR.otf) format('opentype');
   }
-  @font-face {
-    font-family: 'SegoeUI';
-    font-style: normal;
-    font-weight: normal;
-    src: url(assets/font/SegoeUI/SegoeUI.woff) format('woff'); 
-  }
-
   @font-face {
     font-family: 'SegoeUI';
     font-style: normal;
@@ -66,7 +67,11 @@ export const Main = styled.main`
   -webkit-user-select: none;
   user-select: none;
 
-  @media screen and (max-width: 630px) {
+  @media screen and (min-width: 768px) {
+    min-width: 320px;
+  }
+
+  @media screen and (max-width: ${MobileViewWidth}) {
     min-width: 320px;
     height: inherit;
   }
